faec3ab9bd27c1439df3e52bfa3f0da81272e409
parkyes90 <parkyes90@gmail.com>
2020-05-14 22:16:04 +0900

    complete ch04 -5

---
+++ chapter04-5/indexable-key.ts
@@ -0,0 +1,10 @@
+import { makePerson } from "./default"
+
+export type KeyValueType = {
+    [key: string]: string
+}
+
+export const makeObject = (key: string, value: string): KeyValueType => ({[key]: value})
+
+console.log(makeObject("name", "Jack"))
+console.log(makeObject("firstName", "Jack"))
