72485b2e0ef5e49a6f984425328a9b7150f7b5eb
parkyes90 <parkyes90@gmail.com>
2020-06-01 23:45:15 +0900

    add map, reduce

--- chapter08/compose.ts
+++ chapter08/compose.ts
@@ -7,3 +7,6 @@ export const pipe = <T, R> (...functions: readonly Function[]): Function => (x:T
 (T) => R => {
     return functions.reduce((value, func) => func(value),  x)
 };
+
+export const map = f => a => a.map(f)
+export const reduce = <T>(f: (sum:T, value:T) => T, initValue: T) => (a: T[]): T => a.reduce(f, initValue);
