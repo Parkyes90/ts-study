3cac2149e3ca29140e971fad403cf894c78dac93
parkyes90 <parkyes90@gmail.com>
2020-06-07 23:30:04 +0900

    complete ch11

--- chapter08/compose.ts
+++ chapter08/compose.ts
@@ -1,12 +1,17 @@
-export const compose = <T, R>(...functions: readonly Function[]): Function => (x:T): (T) => R => {
-    const deepCopiedFunctions = [...functions]
-    return deepCopiedFunctions.reverse().reduce((value, func) => func(value), x)
-}
-
-export const pipe = <T, R> (...functions: readonly Function[]): Function => (x:T):
-(T) => R => {
-    return functions.reduce((value, func) => func(value),  x)
+export const compose = <T, R>(...functions: readonly Function[]): Function => (
+  x: T
+): ((T) => R) => {
+  const deepCopiedFunctions = [...functions];
+  return deepCopiedFunctions.reverse().reduce((value, func) => func(value), x);
 };
 
-export const map = f => a => a.map(f)
-export const reduce = <T>(f: (sum:T, value:T) => T, initValue: T) => (a: T[]): T => a.reduce(f, initValue);
+export const pipe = <T, R>(...functions: readonly Function[]): Function => (
+  x: T
+): ((T) => R) => {
+  return functions.reduce((value, func) => func(value), x);
+};
+
+export const map = (f) => (a) => a.map(f);
+export const reduce = <T>(f: (sum: T, value: T) => T, initValue: T) => (
+  a: T[]
+): T => a.reduce(f, initValue);
