b725aa41df743bcab0571e5252aecc3c71df7de9
parkyes90 <parkyes90@gmail.com>
2020-06-08 23:48:44 +0900

    add code

---
+++ ch12/src/test/getFileNameAndNumber-test.ts
@@ -0,0 +1,8 @@
+import { getFileNameAndNumber } from '../utils/getFileNameAndNumber';
+
+const [filename, numberOfFakeItems] = getFileNameAndNumber(
+  'data/fake.csv',
+  100000,
+);
+
+console.log(filename, numberOfFakeItems);
---
+++ ch12/src/test/mkdir-test.ts
@@ -0,0 +1,8 @@
+import { mkdir } from '../fileApi/mkdir';
+
+const makeDataDir = async (dirname: string) => {
+  const result = await mkdir(dirname);
+  console.log(`${result} dir created`);
+};
+
+makeDataDir('./data/today').then();
