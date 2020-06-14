fa689ab7e9a155a1b0a215f5b5913d2ae9357228
parkyes90 <parkyes90@gmail.com>
2020-06-14 21:18:33 +0900

    add file api

---
+++ ch12/src/fileApi/index.ts
@@ -0,0 +1,7 @@
+export { fileExists } from './fileExists';
+export { mkdir } from './mkdir';
+export { rmdir } from './rmdir';
+export { writeFile } from './writeFile';
+export { readFile } from './readFile';
+export { appendFile } from './appendFile';
+export { deleteFile } from './deleteFile';
